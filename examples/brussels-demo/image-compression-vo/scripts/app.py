c69fd56be10b316372b96b60af962ba33bf0ac64
Stefane Fermigier <sf@abilian.com>
2025-06-20 16:58:40 +0200

    chore: format examples.

--- examples/brussels-demo/image-compression-vo/scripts/app.py
+++ examples/brussels-demo/image-compression-vo/scripts/app.py
@@ -7,29 +7,26 @@ from PIL import Image
 
 
 def forward_request(buffer):
-    dns_name = 'noise-reduction.default.svc.clusterset.local'
-    url = f'http://{dns_name}:8080/'
+    dns_name = "noise-reduction.default.svc.clusterset.local"
+    url = f"http://{dns_name}:8080/"
     files = {
-        'image': buffer.getvalue(),
+        "image": buffer.getvalue(),
     }
 
     requests.post(url, files=files)
 
 
 async def detectImage_handler(params):
-    input = params['input']
-    image = input['image']
+    input = params["input"]
+    image = input["image"]
 
-    image_bytes = base64.b64decode(image.encode('ascii'))
+    image_bytes = base64.b64decode(image.encode("ascii"))
     img = Image.open(io.BytesIO(image_bytes))
 
     buffer = io.BytesIO()
-    img.save(buffer, format='jpeg', optimize=True, quality=50)
+    img.save(buffer, format="jpeg", optimize=True, quality=50)
 
-    thread = threading.Thread(
-        target=forward_request,
-        args=(buffer,)
-    )
+    thread = threading.Thread(target=forward_request, args=(buffer,))
     thread.start()
 
-    return 'Request forwarded'
+    return "Request forwarded"
--- examples/hello-world-demo/src/hello-world/app.py
+++ examples/hello-world-demo/src/hello-world/app.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
