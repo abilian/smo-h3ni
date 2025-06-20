23584d17197a9f52a2d614436c14220b031e3e4f
Stefane Fermigier <sf@abilian.com>
2025-06-20 17:01:45 +0200

    test: proper nox/uv integration (hopefully)

--- noxfile.py
+++ noxfile.py
@@ -7,16 +7,24 @@ PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session
-def lint(session: nox.Session) -> None:
-    session.run("uv", "sync")
-    session.run("uv", "pip", "check")
-    session.run("uv", "run", "make", "lint")
-    session.run("uv", "pip", "install", "safety", "pip-audit")
-    session.run("uv", "run", "adt", "audit")
+@nox.session(python=PYTHON_VERSIONS[0], venv_backend="uv")
+def lint(session: nox.Session):
+    install_env(session)
+    session.run("ruff", "check", *session.posargs)
 
 
-@nox.session(python=PYTHON_VERSIONS)
-def pytest(session: nox.Session) -> None:
-    session.run("uv", "sync")
-    session.run("uv", "run", "pytest", "--tb=short")
+@nox.session(python=PYTHON_VERSIONS, venv_backend="uv")
+def pytest(session: nox.Session):
+    install_env(session)
+    session.run("pytest", *session.posargs)
+
+
+# Based on https://nox.thea.codes/en/stable/cookbook.html#using-a-lockfile
+def install_env(session: nox.Session):
+    session.run_install(
+        "uv",
+        "sync",
+        "--frozen",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
